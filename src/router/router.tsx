4d6ddc7c16f587695b19e30aef30cf3303371fbf
sanyuchilas <sanyuchilas@gmail.com>
2024-04-16 08:43:01 +0300

    created CodePage

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 
-import { AUTH_PAGE, HOME_PAGE } from "./paths";
+import { AUTH_PAGE, CODE_PAGE, HOME_PAGE } from "./paths";
 
 import HomePage from "../pages/HomePage/HomePage";
 import AuthPage from "../pages/AuthPage/AuthPage";
 import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
+import CodePage from "../pages/CodePage/CodePage";
 
 export const router = createBrowserRouter([
     {
@@ -16,4 +17,8 @@ export const router = createBrowserRouter([
         path: AUTH_PAGE,
         element: <AuthPage />,
     },
+    {
+        path: CODE_PAGE,
+        element: <CodePage />,
+    }
 ]);
