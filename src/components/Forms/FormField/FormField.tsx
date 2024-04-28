4e51180364d5ba53930cbe11fe32dcdc725a8c66
sanyuchilas <sanyuchilas@gmail.com>
2024-04-28 21:52:08 +0300

    added FormField

---
+++ src/components/Forms/FormField/FormField.tsx
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+
+const FormField = () => {
+    useEffect(() => {
+
+    }, []);
+
+    return (
+        <div data-formId={}>
+            {children}
+        </div>    
+    );
+};
+
+export default FormField;
