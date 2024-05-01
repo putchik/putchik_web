8d4f8124795bd4074c09d919e17b3a8ebf7a7013
sanyuchilas <sanyuchilas@gmail.com>
2024-05-02 01:53:28 +0300

    added correct onFormSubmitEnd for AuthForm

--- src/components/Forms/AuthForm/hooks/useAuthForm.ts
+++ src/components/Forms/AuthForm/hooks/useAuthForm.ts
@@ -1,9 +1,19 @@
 import { useForm } from "../../Form/hooks/useForm";
-import { OnFormSubmitStart, UseFormResult } from "../../Form/hooks/types";
+import { OnFormSubmitEnd, OnFormSubmitStart, UseFormResult } from "../../Form/hooks/types";
+import { useNavigate } from "react-router-dom";
+import { CODE_PAGE } from "../../../../router/paths";
 
 export const useAuthForm = (): UseFormResult => {
+    const navigate = useNavigate();
+    
     const onFormSubmitStart: OnFormSubmitStart = () => {
         return new Promise((res) => setTimeout(() => res(null), 1000));
+    };
+
+    const onFormSubmitEnd: OnFormSubmitEnd = (event) => {
+        event.preventDefault();
+
+        navigate(CODE_PAGE);
     };
 
     const {
@@ -17,6 +27,7 @@ export const useAuthForm = (): UseFormResult => {
             useTermsField: false,
         },
         onFormSubmitStart,
+        onFormSubmitEnd,
     });
 
     return {
