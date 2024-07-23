9ae0f08d66f756cda2252dd032858c3ea4ea9b0b
machere13 <vichubarov@edu.hse.ru>
2024-07-24 02:02:09 +0300

    added orders + repeat order + unfixed editing order

--- src/components/Orders/OrderCard.tsx
+++ src/components/Orders/OrderCard.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import styles from "./OrdersLog.module.css";
 import container_styles from '../../UI/containers.module.css';
 import calendar from '../../assets/icons/Calendar.svg';
@@ -14,52 +15,66 @@ import OrderStatus from "./OrderStatus";
 import formatDate from "../dateFormatter";
 
 const OrderCard = (props: { order: Order }) => {
-    return (
-        <div className={styles.centeredFrameSmall}>
-            <div className={styles.infoAndPriceContainer}>
-                <div className={cn(container_styles.flex_col, container_styles.gap_20)} style={{ maxWidth: 'none' }} >
-                    <div className={styles.cardHeaderContainer}>
-                        <div className={cn(container_styles.flex_row, container_styles.gap_20)}>
-                            <h3>{props.order.readable_id}</h3>
-                            <div className={cn(styles.text, styles.date)} >
-                                <img className={styles.calendarImage} src={calendar} />
-                                {formatDate(props.order.created_at)}
-                            </div>
-                        </div>
-                        <OrderStatus status={props.order.status} />
-                    </div>
+  const navigate = useNavigate();
 
-                    <div className={cn(container_styles.flex_row, container_styles.gap_10)} style={{ maxWidth: 500, flexWrap: 'wrap' }}>
-                        <RedBox startImgSrc={path} text={`${props.order.distance.toString()} км`} done={props.order.status == "Delivered"} />
-                        <RedBox startImgSrc={volume} text={`${props.order.amount} м³`} done={props.order.status == "Delivered"} />
-                        <RedBox startImgSrc={weight} text={`${props.order.weight} кг`} done={props.order.status == "Delivered"} />
-                        <RedBox startImgSrc={cargo} text={props.order.cargo} done={props.order.status == "Delivered"} />
-                    </div>
+  const handleEditOrder = () => {
+    navigate('/order', { state: { order: props.order, mode: 'edit' } });
+  };
 
-                    <div className={styles.pathContainer}>
-                        <div>
-                            <div style={{ fontWeight: 500, fontSize: '15px', lineHeight: '17px' }}>{props.order.loading_points[0].locality}</div>
-                            <div className="hint" style={{ wordBreak: 'break-word' }}>{props.order.loading_points[0].address}</div>
-                        </div>
-                        <img src={pathArrow} className={styles.arrowImage} />
-                        <div>
-                            <div style={{ fontWeight: 500, fontSize: '15px', lineHeight: '17px' }}>{props.order.unloading_points[0].locality}</div>
-                            <div className="hint" style={{ wordBreak: 'break-word' }}>{props.order.unloading_points[0].address}</div>
-                        </div>
-                    </div>
-                </div>
+  const handleRepeatOrder = () => {
+    navigate('/order', { state: { order: props.order, mode: 'repeat' } });
+  };
 
-                <div className={styles.priceAndRepeatContainer}>
-                    <div style={{ marginBlock: 'auto' }}>
-                        <h2 className={styles.price}>{`${props.order.cost} ₽`}</h2>
-                        <div className="hint">с НДС</div>
-                    </div>
+  const distanceInKm = Math.floor(props.order.distance / 1000);
 
-                    <div className={styles.redText}>Повторить</div>
-                </div>
+  return (
+    <div className={styles.centeredFrameSmall}>
+      <div className={styles.infoAndPriceContainer} onClick={handleEditOrder}>
+        <div className={cn(container_styles.flex_col, container_styles.gap_20)} style={{ maxWidth: 'none' }} >
+          <div className={styles.cardHeaderContainer}>
+            <div className={cn(container_styles.flex_row, container_styles.gap_20)}>
+              <h3>{props.order.readable_id}</h3>
+              <div className={cn(styles.text, styles.date)} >
+                <img className={styles.calendarImage} src={calendar} />
+                {formatDate(props.order.created_at)}
+              </div>
             </div>
+            <OrderStatus status={props.order.status} />
+          </div>
+
+          <div className={cn(container_styles.flex_row, container_styles.gap_10)} style={{ maxWidth: 500, flexWrap: 'wrap' }}>
+            <RedBox startImgSrc={path} text={`${distanceInKm} км`} done={props.order.status === "Delivered"} />
+            <RedBox startImgSrc={volume} text={`${props.order.amount} м³`} done={props.order.status === "Delivered"} />
+            <RedBox startImgSrc={weight} text={`${props.order.weight} кг`} done={props.order.status === "Delivered"} />
+            <RedBox startImgSrc={cargo} text={props.order.cargo} done={props.order.status === "Delivered"} />
+          </div>
+
+          <div className={styles.pathContainer}>
+            <div>
+              <div style={{ fontWeight: 500, fontSize: '15px', lineHeight: '17px' }}>{props.order.loading_points[0].locality}</div>
+              <div className="hint" style={{ wordBreak: 'break-word' }}>{props.order.loading_points[0].address}</div>
+            </div>
+            <img src={pathArrow} className={styles.arrowImage} />
+            <div>
+              <div style={{ fontWeight: 500, fontSize: '15px', lineHeight: '17px' }}>{props.order.unloading_points[0].locality}</div>
+              <div className="hint" style={{ wordBreak: 'break-word' }}>{props.order.unloading_points[0].address}</div>
+            </div>
+          </div>
         </div>
-    );
+
+        <div className={styles.priceAndRepeatContainer}>
+          <div style={{ marginBlock: 'auto' }}>
+            <h2 className={styles.price}>{`${props.order.cost} ₽`}</h2>
+            <div className="hint">с НДС</div>
+          </div>
+
+          <div className={styles.redText} onClick={(e) => { e.stopPropagation(); handleRepeatOrder(); }}>
+            Повторить
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default OrderCard;
--- src/pages/OrdersPage/OrdersPage.tsx
+++ src/pages/OrdersPage/OrdersPage.tsx
@@ -1,87 +1,49 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import styles from "./OrdersPage.module.css";
 import Header from "../../components/Header/Header";
 import Order, { OrderResponse } from "../../components/Orders/OrderModel";
 import OrdersLog from "../../components/Orders/OrdersLog";
-import { useEffect, useState } from "react";
 import fetchGetUserOrders from "../../fetch_functions/fetchGetUserOrders";
 
 const OrdersPage = () => {
-    useEffect(() => {
-        fetchGetUserOrders()
-            .then((data: OrderResponse) => {
-                console.log(data);
-                setOrdersList(data.orders);
-            })
-    }, [])
+  const [ordersList, setOrdersList] = useState<Order[]>([]);
+  const location = useLocation();
 
-    const [ordersList, setOrdersList] = useState<Order[]>()
+  useEffect(() => {
+    fetchGetUserOrders()
+      .then((data: OrderResponse) => {
+        console.log(data);
+        setOrdersList(data.orders);
+      });
+  }, []);
 
-    // const ordersList: Order[] = [
-    //     {
-    //         id: 1,
-    //         readable_id: "#FW21244",
-    //         customer_id: 1,
-    //         cargo: "Чемоданы",
-    //         created_at: "2024-07-14T10:51:39.533Z",
-    //         cost: 15000,
-    //         distance: 1220,
-    //         weight: 5,
-    //         amount: 2,
-    //         temperature_condition: true,
-    //         status: "Transit",
-    //         loading_points: [
-    //             {
-    //                 locality: "Москва",
-    //                 address: "Невский проспект 1",
-    //                 phone: "+79999999999"
-    //             }
-    //         ],
-    //         unloading_points: [
-    //             {
-    //                 locality: "Санкт-Петербург",
-    //                 address: "Невский проспект 1",
-    //                 phone: "+79999999999"
-    //             }
-    //         ]
-    //     },
-    //     {
-    //         id: 1,
-    //         readable_id: "#FW21245",
-    //         customer_id: 1,
-    //         cargo: "Чемоданы",
-    //         created_at: "2024-07-14T10:51:39.533Z",
-    //         cost: 15000,
-    //         distance: 1220,
-    //         weight: 5,
-    //         amount: 2,
-    //         temperature_condition: true,
-    //         status: "done",
-    //         loading_points: [
-    //             {
-    //                 locality: "Москва",
-    //                 address: "Невский проспект 1",
-    //                 phone: "+79999999999"
-    //             }
-    //         ],
-    //         unloading_points: [
-    //             {
-    //                 locality: "Санкт-Петербург",
-    //                 address: "Невский проспект 1",
-    //                 phone: "+79999999999"
-    //             }
-    //         ]
-    //     }
-    // ]
+  useEffect(() => {
+    if (location.state) {
+      const newOrder = location.state.newOrder as Order;
+      const updatedOrder = location.state.updatedOrder as Order;
 
-    return (
-        <div className={styles.page}>
-            <Header />
-            {ordersList ?
-                <OrdersLog orderList={ordersList} />
-                : <h3>История заявок пуста</h3>
-            }
-        </div>
-    );
+      if (newOrder) {
+        setOrdersList(prevOrders => [...prevOrders, newOrder]);
+      }
+
+      if (updatedOrder) {
+        setOrdersList(prevOrders =>
+          prevOrders.map(order => (order.id === updatedOrder.id ? updatedOrder : order))
+        );
+      }
+    }
+  }, [location.state]);
+
+  return (
+    <div className={styles.page}>
+      <Header />
+      {ordersList.length > 0 ?
+        <OrdersLog orderList={ordersList} />
+        : <h3>История заявок пуста</h3>
+      }
+    </div>
+  );
 };
 
 export default OrdersPage;
