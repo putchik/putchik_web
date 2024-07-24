0e7cfeb6f58204aaec1e364ab407665ddb1e7f0f
Daria Lunkova <dvlunkova@edu.hse.ru>
2024-07-24 12:03:16 +0300

    changed path to main page

--- src/router/paths.ts
+++ src/router/paths.ts
@@ -1,8 +1,7 @@
-export const HOME_PAGE = "/";
+export const MAIN_PAGE = "/";
 export const AUTH_PAGE = "/auth";
 export const LOADER_PAGE = "/loader";
 export const ORDER_PAGE = "/order"
-export const MAIN_PAGE = "/main"
 export const ORDERS_PAGE = "/orders";
 export const PROFILE_PAGE = "/profile";
 export const ADMIN_AUTH_PAGE = "/admin_auth";
